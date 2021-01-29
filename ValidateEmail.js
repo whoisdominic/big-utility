39dd2c5d20f025a399ab9f00077ec00e144b958a
Dominic <32504814+whoisdominic@users.noreply.github.com>
2021-01-28 18:52:18 -0800

    Update ValidateEmail.js

--- ValidateEmail.js
+++ ValidateEmail.js
@@ -1,9 +1,3 @@
-function ValidateEmail(mail) 
-{
- if (/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(myForm.emailAddr.value))
-  {
-    return {result: true, msg: "Email format is valid!"}
-  }
-    alert()
-    return {result: false, msg: "You have entered an invalid email address!"}
+export function ValidateEmail (email) {
+  return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)
 }
